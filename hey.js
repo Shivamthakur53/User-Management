9572b4b06bf9a24e0611319925e66e7b5ece4240
anubhav-uchiha <anubhav27km@gmail.com>
2024-01-16 13:42:26 +0530

    ,,,

--- hey.js
+++ hey.js
@@ -1,30 +1,30 @@
-// console.log("this is my file");
-// console.log("dd");
+console.log("this is my file");
+console.log("dd");
 
-// console.log("tewsting");
-// console.log("kjd");
-// import React, { useState } from "react";
+console.log("tewsting");
+console.log("kjd");
+import React, { useState } from "react";
 
-// const hey = () => {
-//   const [value, setvalue] = useState;
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-//   console.log("hy anubhav");
-//   return <div></div>;
-// };
+const hey = () => {
+  const [value, setvalue] = useState;
+<<<<<<< HEAD
+<<<<<<< HEAD
+  console.log("hy anubhav");
+  return <div></div>;
+};
 
-// export default hey;
-// =======
-//   return <div>vewulcqecoqeulci</div>;
-// =======
-//   return (
-//     <div>
-//       <h1>hello world</h1>
-//     </div>
-//   );
-// >>>>>>> 6bc5239f2b74e2fe559e27295505aa0e72c0a66f
-// };
+export default hey;
+=======
+  return <div>vewulcqecoqeulci</div>;
+=======
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+>>>>>>> 6bc5239f2b74e2fe559e27295505aa0e72c0a66f
+};
 
-// export default hey;
-// console.log("hy anubhav");
-// >>>>>>> f3006c9a688f7849ad756bb936073ed4be6b4d5f
+export default hey;
+console.log("hy anubhav");
+>>>>>>> f3006c9a688f7849ad756bb936073ed4be6b4d5f
