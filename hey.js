f5ecda4795daedc5ab89a0a0953730a7069cc4c1
anubhav-uchiha <anubhav27km@gmail.com>
2024-01-16 13:39:25 +0530

    ...

--- hey.js
+++ hey.js
@@ -3,15 +3,12 @@ console.log("dd");
 
 console.log("tewsting");
 console.log("kjd");
-<<<<<<< HEAD
 import React, { useState } from "react";
 
 const hey = () => {
   const [value, setvalue] = useState;
+  console.log("hy anubhav");
   return <div></div>;
 };
 
 export default hey;
-=======
-console.log("hy anubhav");
->>>>>>> 274c0dd18a2b3a27979476728bd76bfaf89b0f6f
