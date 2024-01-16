53e52734cf4e85d9fa29e7fec8b759497f63d0cb
anubhav-uchiha <anubhav27km@gmail.com>
2024-01-16 13:47:22 +0530

    meri tarf se

--- hey.js
+++ hey.js
@@ -1,3 +1,10 @@
+console.log("this is my file");
+console.log("dd");
+
+console.log("tewsting");
+console.log("kjd");
+import React, { useState } from "react";
+
 const hey = () => {
   const [value, setvalue] = useState;
   console.log("hy anubhav");
